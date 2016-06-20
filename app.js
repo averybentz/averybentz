703663f0434c337edfebeb67412764997ef0b893
Avery Bentz <averybentz@bell.net>
2016-06-19 20:29:53 -0400

    deploying to heroku

---
+++ app.js
@@ -0,0 +1,31 @@
+/*
+ * Module dependencies
+ */
+var express = require('express')
+
+var app = express()
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'html')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+//Run
+app.get('/', function (req, res) {
+  res.end('Hi there!')
+}) 
+/*
+app.get('/', function (req, res) {
+  res.render('index'),
+  { title: 'Home' }
+  )
+})*/
