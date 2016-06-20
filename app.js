6dc36ff3f11e211d5800e212de9fe1ac684ebdb8
Avery Bentz <averybentz@bell.net>
2016-06-19 22:02:37 -0400

    install connect

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /*
+/*
  * Module dependencies
- */
+//close bracket
 var express = require('express')
 
 var app = express()
@@ -17,10 +18,10 @@ app.use(express.logger('dev'))
   , compile: compile
   }
 ))*/
-app.use(express.static(__dirname + '/public'))
+//app.use(express.static(__dirname + '/public'))
 
 //Run
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.send('Hi there!')
   res.end('hi')
 }) 
@@ -29,6 +30,33 @@ app.get('/', function (req, res) {
   res.render('index'),
   { title: 'Home' }
   )
-})*/
+})
 
-app.listen(3000)
+app.listen(3000)*/
+
+var connect = require('connect');
+var http = require('http');
+
+var app = connect();
+
+// gzip/deflate outgoing responses
+var compression = require('compression');
+app.use(compression());
+
+// store session state in browser cookie
+var cookieSession = require('cookie-session');
+app.use(cookieSession({
+    keys: ['secret1', 'secret2']
+}));
+
+// parse urlencoded request bodies into req.body
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
+
+// respond to all requests
+app.use(function(req, res){
+  res.end('Hello from Connect!\n');
+});
+
+//create node.js http server and listen on port
+http.createServer(app).listen(3000);
